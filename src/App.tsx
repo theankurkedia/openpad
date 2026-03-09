abcb4c8f724328d75596018a5c76dd2bd0ad0853
Ankur Kedia <theankurkedia@gmail.com>
2026-03-09 21:43:17 +0100

    feat: added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import { ActionArea, ShortcutModal } from './components';
 
@@ -10,6 +11,7 @@ function App() {
       </div>
       <ActionArea />
       <ShortcutModal />
+      <Analytics />
     </div>
   );
 }
